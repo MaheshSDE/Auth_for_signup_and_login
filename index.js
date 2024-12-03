010efe5f66650ca11de1036123a075636f904695
mahesh <pmahesh493605@gmail.com>
2024-12-03 11:05:47 +0530

    add exceptional handling

--- index.js
+++ index.js
@@ -152,94 +152,108 @@ app.get("/",(request,response)=>{
 //Create Account
 app.post("/create-account",async(request,response)=>{
     const {name,emailInput,passwordInput}=request.body
-
-    if(!name){
-        return response.status(400).json({error:true, message:"Full name is Required"});
-    }
-
-    if(!emailInput){
-        return response.status(400).json({error:true, message:"Email is Required"});
-    }
-
-    if(!passwordInput){
-        return response.status(400).json({error:true, message:"Password is Required"});
-    }
-
-    const isUser=await User.findOne({emailInput:emailInput})
+    try{
+        if(!name){
+            return response.status(400).json({error:true, message:"Full name is Required"});
+        }
     
-    if(isUser){
-         response.json({error:true,message:"User Already Exist!!!"});
+        if(!emailInput){
+            return response.status(400).json({error:true, message:"Email is Required"});
+        }
+    
+        if(!passwordInput){
+            return response.status(400).json({error:true, message:"Password is Required"});
+        }
+    
+        const isUser=await User.findOne({emailInput:emailInput})
+        
+        if(isUser){
+             response.json({error:true,message:"User Already Exist!!!"});
+        }
+        else{
+            /* const user=new User({
+                name,
+                emailInput,
+                passwordInput,
+            });
+            await user.save() */
+    
+            const hashedPassword = await bcrypt.hash(passwordInput,10);
+            const user=await User({
+                name,
+                emailInput,
+                passwordInput:hashedPassword,
+            })
+            await user.save()
+    
+           /*  const accessToken=jwt.sign({user},"MY_SECRETE_TOKEN",{expiresIn:"36000m"})
+            response.json({
+                error:false,
+                user,
+                accessToken,
+                message:"Registration Successful!!!",
+            }) */
+            response.status(200).json({ error: false, user,message: "User created successfully." });
+          
+        }
     }
-    else{
-        /* const user=new User({
-            name,
-            emailInput,
-            passwordInput,
-        });
-        await user.save() */
-
-        const hashedPassword = await bcrypt.hash(passwordInput,10);
-        const user=await User({
-            name,
-            emailInput,
-            passwordInput:hashedPassword,
-        })
-        await user.save()
-
-       /*  const accessToken=jwt.sign({user},"MY_SECRETE_TOKEN",{expiresIn:"36000m"})
-        response.json({
-            error:false,
-            user,
-            accessToken,
-            message:"Registration Successful!!!",
-        }) */
-        response.status(200).json({ error: false, user,message: "User created successfully." });
-      
+    catch(error){
+        console.log(error);
+        res.status(500).json({error:true, message:"Internal Server Error",error});
     }
+
+    
 })
 
 //Create Login 
 app.post('/login',async (request,response)=>{
     const {emailInput,passwordInput}=request.body
-
-    if(!emailInput){
-        return response.status(400).json({message:"Email is Required"});
-    }
-
-    if(!passwordInput){
-        return response.status(400).json({message:"Password is Required"});
-    }
-
-    const userInfo=await User.findOne({emailInput:emailInput})
-
-    if(!userInfo){
-        response.status(400).json({error:true,message:"User Not Found or Invalid User"});
-    }
-    else{
-        /* if(userInfo.emailInput===emailInput && userInfo.passwordInput===passwordInput){
-            const user={user:userInfo};
-            const accessToken=jwt.sign(user,"MY_SECRETE_TOKEN",{expiresIn:"36000m"});
-            response.json({
-                error:false,
-                emailInput,
-                accessToken,
-                message:"Login Successful!!!",
-            })
-        }else{
-            response.status(400).json({
-                error:true,
-                message:"Invalid Credentials"
-            })
-        } */
-
-        const isPasswordMatched = await bcrypt.compare(passwordInput,userInfo.passwordInput);
-        if(userInfo.emailInput===emailInput && isPasswordMatched){
-            const payload={emailInput:emailInput};
-            const jwtSecret= "default_secret"
-            const jwtToken=jwt.sign(payload, jwtSecret);
-            response.status(200).json({ error: false,email, jwtToken,message:"Login Successful" });  
+    try {
+        if(!emailInput){
+            return response.status(400).json({message:"Email is Required"});
         }
+    
+        if(!passwordInput){
+            return response.status(400).json({message:"Password is Required"});
+        }
+    
+        const userInfo=await User.findOne({emailInput:emailInput})
+    
+        if(!userInfo){
+            response.status(400).json({error:true,message:"User Not Found or Invalid User"});
+        }
+        else{
+            /* if(userInfo.emailInput===emailInput && userInfo.passwordInput===passwordInput){
+                const user={user:userInfo};
+                const accessToken=jwt.sign(user,"MY_SECRETE_TOKEN",{expiresIn:"36000m"});
+                response.json({
+                    error:false,
+                    emailInput,
+                    accessToken,
+                    message:"Login Successful!!!",
+                })
+            }else{
+                response.status(400).json({
+                    error:true,
+                    message:"Invalid Credentials"
+                })
+            } */
+    
+            const isPasswordMatched = await bcrypt.compare(passwordInput,userInfo.passwordInput);
+            if(userInfo.emailInput===emailInput && isPasswordMatched){
+                const payload={emailInput:emailInput};
+                const jwtSecret= "default_secret"
+                const jwtToken=jwt.sign(payload, jwtSecret);
+                response.status(200).json({ error: false,email, jwtToken,message:"Login Successful" });  
+            }
+        }
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:true, message:"Internal Server Error",error});
     }
+
+    
 })
 
 const PORT = process.env.PORT || 4005
